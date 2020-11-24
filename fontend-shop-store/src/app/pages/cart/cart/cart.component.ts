cb0113ac2b0f7f54c188f77cde99ddae3ea96f51
anhdt251196 <anhdt251196@gmail.com>
2020-11-24 18:00:50 +0700

    add to cart

--- fontend-shop-store/src/app/pages/cart/cart/cart.component.ts
+++ fontend-shop-store/src/app/pages/cart/cart/cart.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductInfo } from 'src/app/models/ProductInfo';
+import { ProductInOrder } from 'src/app/models/ProductInOrder';
+import { CartService } from 'src/app/services/cart.service';
+import { ValidationUtils } from 'src/app/utils/ValidationUtils';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent implements OnInit {
+export class CartComponent implements OnInit, OnDestroy, AfterContentChecked {
 
-  constructor() { }
+  productsInOrder : ProductInOrder[]=  [];
+  title = ""
+  price = 0;
+
+  constructor(private cartService: CartService, private router: Router) { 
+    
+  }
+  ngOnDestroy(): void {
+  }
+  ngAfterContentChecked(): void {
+    this.calculateTotalPrice();
+  }
 
   ngOnInit(): void {
+    this.getProductsInOrder();
+    this.title = "My Cart"
+  }
+
+  getProductsInOrder() {
+    this.cartService.getCart().subscribe(res => {
+      this.productsInOrder = res;
+    })
+  }
+
+  onChange(productInOrder: ProductInOrder) {
+    ValidationUtils.validateCount(productInOrder);
+  }
+
+  calculateTotalPrice() {
+    let localPrice = 0;
+    this.productsInOrder.forEach((prodInOrder, index) => {
+      localPrice += prodInOrder.productPrice * prodInOrder.count;
+    })
+    this.price = localPrice;
+  }
+
+  checkout() {
+    console.log('checkout');
+  }
+
+  remove(productInOrder: ProductInOrder) {
+    this.cartService.remove(productInOrder)
+    .subscribe(res =>{
+      console.log('remove sucssesfully')
+    }, error =>{
+      console.log('remove failed')
+    })
   }
 
 }
--- fontend-shop-store/src/app/services/cart.service.ts
+++ fontend-shop-store/src/app/services/cart.service.ts
@@ -28,4 +28,34 @@ export class CartService {
       this.cookieStore.set('cart', JSON.stringify(this.localMap));
       return of(true);
   }
+
+  getCart() : Observable<ProductInOrder[]> {
+    const localCart = this.getLocalCart();
+    return of(localCart);
+  }
+
+  remove(productInOrder: ProductInOrder) : Observable<boolean>{
+    if(this.cookieStore.check('cart')) {
+      this.localMap = JSON.parse(this.cookieStore.get('cart'));
+    }
+
+    if(!this.localMap[productInOrder.productId]) {
+        return of(false);
+    } else {
+      delete this.localMap[productInOrder.productId]
+    }
+    this.cookieStore.set('cart', JSON.stringify(this.localMap));
+    return of(true);
+  }
+
+  private getLocalCart(): ProductInOrder[] {
+    if(this.cookieStore.check('cart')) {
+      this.localMap = JSON.parse(this.cookieStore.get('cart'));
+      return Object.values(this.localMap);
+    } else {
+      this.localMap = {};
+      return [];
+    }
+  }
+
 }
