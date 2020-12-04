01ac6f81b52026acb7c359b20477e3510be38a68
anhdt251196 <anhdt251196@gmail.com>
2020-12-04 17:52:21 +0700

    do ui and validate login

--- fontend-shop-store/src/app/services/user.service.ts
+++ fontend-shop-store/src/app/services/user.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { apiUrl } from 'src/environments/environment';
 import { User } from '../models/User';
+import { UserResponse } from '../response/UserResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -18,8 +20,13 @@ export class UserService {
     return this.http.post<User>(url, user);
   }
 
-  login(user) : Observable{
-    
+  login(userForm) : Observable<UserResponse>{
+    const url = `${apiUrl}/login`;
+    return this.http.post<UserResponse>(url, userForm).pipe(
+      tap(user => {
+        
+      })
+    );
   }
   
 }
